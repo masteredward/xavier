f364e6fc259ac4d36625db388b9e38f85a6bf374
Eduardo Medeiros Silva <edu.medeiros.info@gmail.com>
2022-05-28 13:18:38 +0000

    Changed both Kind and Xavier's /root mounts

--- run_xavier.sh
+++ run_xavier.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+docker stop xavier
+docker rm xavier
+
 docker run -d \
   --name xavier \
   --hostname xavier \
   --privileged \
   -p 2222:22 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /home/core/xavier:/root \
-  -v /kind:/kind \
+  -v /storage/xavier:/root \
+  -v /storage/kind:/kind \
   --restart unless-stopped \
   xavier
